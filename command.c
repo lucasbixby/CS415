90e25eb1d45e49cec7aac53115b5a5f693d1d79f
Lucas Bixby <lucasbixby04@gmail.com>
2026-04-09 17:24:50 -0700

    initialized skeleton code structure

---
+++ command.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void listDir()
+/*for the ls command*/
+{
+    return;
+}
+
+void showCurrentDir()
+/*for the pwd command*/
+{
+    return;
+}
+
+void makeDir(char *dirName)
+/*for the mkdir command*/
+{
+    return;
+}
+
+void changeDir(char *dirName)
+/*for the cd command*/
+{
+    return;
+}
+
+void copyFile(char *sourcePath, char *destinationPath) 
+/*for the cp command*/
+{
+    return;
+}
+
+void moveFile(char *sourcePath, char *destinationPath)
+/*for the mv command*/
+{
+    return;
+}
+
+void deleteFile(char *filename)
+/*for the rm command*/
+{
+    return;
+}
+
+void displayFile(char *filename)
+/*for the cat command*/
+{
+    return;
+}
