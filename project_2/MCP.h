8f7feb844266c08e0cb9ff3f47b01101faa46e9d
Lucas Bixby <lucasbixby04@gmail.com>
2026-05-06 23:40:53 -0700

    centralized the helper funciton in the project header file

--- project_2/MCP.h
+++ project_2/MCP.h
@@ -8,6 +8,131 @@ typedef struct
     char *raw_line; 
 } command_line;
 
-command_line* extract_commands(char *workload_file, int *num_commands);
+//command_line* extract_commands(char *workload_file, int *num_commands);
+
+int count_lines(FILE *fp) 
+// count the lines in an input file 
+{
+    char *line = NULL;
+    size_t len = 0;
+    int count = 0;
+
+    while (getline(&line, &len, fp) != -1) {
+        count++;
+    }
+
+    free(line);
+
+    rewind(fp);
+
+    return count;
+}
+
+char **parse_args(char *line, int *count) 
+// parse the arguments of a command line
+{
+    int capacity = 10;
+
+    char **args = malloc(capacity * sizeof(char *));
+    if (args == NULL) {
+        perror("malloc args");
+        *count = 0;
+        return NULL;
+    }
+
+    *count = 0;
+
+    char *token = strtok(line, " \t\n");
+
+    while (token != NULL) {
+        if (*count >= capacity) {
+            capacity *= 2;
+
+            char **temp = realloc(args, capacity * sizeof(char *));
+            if (temp == NULL) {
+                perror("realloc args");
+                free(args);
+                *count = 0;
+                return NULL;
+            }
+
+            args = temp;
+        }
+
+        args[*count] = token;
+        (*count)++;
+
+        token = strtok(NULL, " \t\n");
+    }
+
+    char **temp = realloc(args, (*count + 1) * sizeof(char *));
+    if (temp == NULL) {
+        perror("realloc final args");
+        free(args);
+        *count = 0;
+        return NULL;
+    }
+
+    args = temp;
+    args[*count] = NULL;
+
+    return args;
+}
+
+command_line* extract_commands(char* workload_file, int *num_commands)
+// tokenizes an input file of commands seperated by newlines 
+// creates an array of commands. all command strings -> individual command arguments 
+{
+    // 1.) open file 
+    // 2.) read each line into a line buffer using getline()
+    // 3.) parse each buffer by " " into a command_line struct (command_line.commands) 
+    //     and count the number of args (command_line.num_arguments)
+    // 4.) store each command_line into the workload array 
+
+    FILE *fp = fopen(workload_file, "r");
+    if (fp == NULL) {
+        perror("fopen input");
+        return NULL; 
+    } 
+
+    int count = count_lines(fp);
+    *num_commands = count; 
+    command_line* workload = malloc(sizeof(command_line) * count);
+
+    char *line = NULL;
+    size_t len = 0; 
+    ssize_t nread; 
+    int i = 0; 
+
+    while ((nread = getline(&line, &len, fp)) != -1) {
+        char *line_copy = strdup(line);
+        if (line_copy == NULL) {
+            perror("strdup");
+            continue;
+        }
+
+        int argc;
+        char **argv = parse_args(line_copy, &argc);
+
+        if (argv == NULL) {
+            free(line_copy);
+            continue;
+        }
+
+        workload[i].command = argv;
+        workload[i].num_arguments = argc;
+
+        workload[i].raw_line = line_copy;
+
+        i++;
+    }
+
+    free(line);
+    fclose(fp);
+
+    *num_commands = i;
+
+    return workload; 
+}
 
 #endif
